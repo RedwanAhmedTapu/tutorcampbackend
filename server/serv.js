011e47eca7e5a751146ed263cb8bd41bcf33736e
RedwanAhmedTapuu <redwan25880@gmail.com>
2024-04-17 21:12:39 +0600

    videomeeting implemented

--- server/serv.js
+++ server/serv.js
@@ -1,10 +1,13 @@
+// server.js
+
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const cors = require("cors");
+const cors=require("cors");
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -42,23 +45,26 @@ io.on("connection", (socket) => {
     io.to(roomId).emit("newjoining", { id: socket.id });
   });
 
-  socket.on("sendTheOffer", (offer, roomId) => {
-    console.log(offer, "offer");
-    io.to(roomId).emit("recieveOffer", offer);
+
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
   });
 
-  socket.on("sendTheAnswer", (ans, roomId) => {
-    console.log(ans, "answer");
-    io.to(roomId).emit("recieveAnswer", ans);
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
   });
 
-  // Receive ICE candidates
-  socket.on("sendIceCandidate", (candidate, roomId) => {
-    console.log("Received ICE candidate:", candidate);
-    io.to(roomId).emit("receiveIceCandidate", candidate);
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
 });
-
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
